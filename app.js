2865f4fe464176ecdc51deff98dd5903334aae9d
IsabelaPenido <isabelapenido.s@gmail.com>
2025-03-17 15:56:08 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = []
+let adicionarAmigo = []
+let sortearAmigo = []
+
+function adicionarAmigos () {
+
+    let nome = document.querySelector ("#amigo").value.trim()
+
+    if (nome = "") {
+        return alert("Por favor, insira um nome.")
+}
+
+}
+
+let amigos = [];
+amigos.push(<ul>
+    <li>Isabela</li>
+    <li>Alice</li>
+    <li>Heloísa</li>
+  </ul>);
+    limparCAmpo();
+    
+function amigos () {
+
+    if(!amigos.length) {
+    return alert("Nenhum nome disponível")
+        
+}
+
+for (let amigos = 0; amigos < array.length; amigos++) {
+    const amigos = array[amigos];
+    
+    
+}
+
+let amigos = document.getElementById(listaAmigos);
+lista.innerHTML == "";
+}    
+
+function sortearAmigo() {
+    let amigos = parseInt(Math.random() * numeroLimite + 1);
+    
+}
