9a1f90c07257b4876a11142a0335d28105821fd1
IsabelaPenido <isabelapenido.s@gmail.com>
2025-03-18 19:33:36 -0300

    UpdateAmigoSecreto.js

--- app.js
+++ app.js
@@ -1,45 +1,32 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let amigos = []
-let adicionarAmigo = []
-let sortearAmigo = []
 
-function adicionarAmigos () {
-
-    let nome = document.querySelector ("#amigo").value.trim()
-
-    if (nome = "") {
-        return alert("Por favor, insira um nome.")
+function adicionarAmigo() {
+    let input = document.querySelector("#amigo");
+    let nome = input.value.trim();
+    if (nome == "") {
+        return alert("Por favor, insira um nome.");
+    }
+    amigos.push(nome);
+    input.value = "";
+    adicionarNaLista();
 }
 
+function adicionarNaLista() {
+    let ul = document.getElementById("listaAmigos");
+    ul.innerHTML = "";
+    amigos.map((amigo) =>{
+        let li = document.createElement("li");
+        li.innerText = amigo            
+        ul.append(li)
+    } );
 }
-
-let amigos = [];
-amigos.push(<ul>
-    <li>Isabela</li>
-    <li>Alice</li>
-    <li>Heloísa</li>
-  </ul>);
-    limparCAmpo();
-    
-function amigos () {
-
-    if(!amigos.length) {
-    return alert("Nenhum nome disponível")
-        
-}
-
-for (let amigos = 0; amigos < array.length; amigos++) {
-    const amigos = array[amigos];
-    
-    
-}
-
-let amigos = document.getElementById(listaAmigos);
-lista.innerHTML == "";
-}    
 
 function sortearAmigo() {
-    let amigos = parseInt(Math.random() * numeroLimite + 1);
-    
+    if (amigos.length == 0) {
+        return alert("O array está vazio");
+    }
+    let randomIndex = Math.floor(Math.random() * amigos.length);
+    document.getElementById("resultado").innerHTML = amigos[randomIndex]
 }
